4b45b360291645924931632fae2fc82881dc73d2
Robin <robin.django@gmail.com>
2024-12-29 16:32:27 +0100

    Made Title responsive on Desktop

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -17,6 +17,7 @@
     position: absolute;
   }
 
+  /* I */
   span.title-1 {
     position: absolute;
     top: 52.5vw;
@@ -25,6 +26,7 @@
     color: var(--black);
   }
 
+  /* Heart */
   h1 :global(svg) {
     position: absolute;
 
@@ -35,11 +37,64 @@
     width: auto;
   }
 
+  /* Web */
   span.title-2 {
     position: absolute;
     top: 77.5vw;
     left: 53vw;
 
     color: var(--white);
+  }
+
+  @media only screen and (min-width: 430px) {
+    h1 {
+      font-size: 11vw;
+    }
+
+    /* I */
+    span.title-1 {
+      top: 25.75vw;
+      left: 51.5vw;
+    }
+
+    /* Heart */
+    h1 :global(svg) {
+      top: 27vw;
+      left: 56.5vw;
+
+      height: 8.5vw;
+    }
+
+    /* Web */
+    span.title-2 {
+      top: 38vw;
+      left: 51.5vw;
+    }
+  }
+
+  @media only screen and (min-width: 992px) {
+    h1 {
+      font-size: 7vw;
+    }
+
+    /* I */
+    span.title-1 {
+      top: 14.35vw;
+      left: 42.5vw;
+    }
+
+    /* Heart */
+    h1 :global(svg) {
+      top: 15.2vw;
+      left: 45.5vw;
+
+      height: 5.4vw;
+    }
+
+    /* Web */
+    span.title-2 {
+      top: 21.45vw;
+      left: 42.5vw;
+    }
   }
 </style>
