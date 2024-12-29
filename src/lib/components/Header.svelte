3ec31ee1590b3f46b8f21f1f822185623632a676
Robin <robin.django@gmail.com>
2024-12-29 16:32:16 +0100

    Completed Header desktop layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,70 +1,57 @@
 <script>
-  import Fluff from "./Fluff.svelte";
   import TileArch from "./TileArch.svelte";
   import TileHorizontal from "./TileHorizontal.svelte";
   import Title from "./Title.svelte";
 </script>
 
-<div>
-<Fluff> 
-  <TileArch class="tablet" />
-  <TileArch class="tablet" />
-  <TileArch class="tablet" />
-</Fluff>
-
 <header>
 
   <!-- 
-  d m m d d
-  d m m d d
+  d t m m t d d
+  d t m m t d d
+  d t m m t d d
   -->
 
   <Title />
 
   <!-- Row 1 -->
-  <TileArch class="desktop" colorBg="red" />
+  <TileArch class="desktop" position="bottomleft" colorBg="red" colorLarge="black" />
+  <TileArch class="tablet" position="bottomright" colorBg="blue" colorLarge="black" />
   <TileArch class="mobile" position="bottomright" colorBg="yellow" colorLarge="black" colorSmall="red" />
-  <TileArch class="mobile" position="topright" colorBg="red" colorLarge="black" colorSmall="black "/>
-  <TileArch class="desktop" colorBg="red"/>
-  <TileArch class="desktop" colorBg="red" />
+  <TileArch class="mobile" position="topright" colorBg="red" colorLarge="black"/>
+  <TileArch class="tablet" position="bottomleft" colorBg="black" colorLarge="blue" />
+  <TileArch class="desktop" position="topleft" colorBg="black" colorLarge="white" />
+  <TileArch class="desktop" position="bottomright" colorBg="yellow" colorLarge="black" />
 
   <!-- Row 2 -->
-  <TileArch class="desktop" colorBg="red" />
+  <TileArch class="desktop" position="topright" colorBg="blue" colorLarge="black" />
+  <TileArch class="tablet" position="topright" colorBg="black" colorLarge="white" colorSmall="white" />
   <TileArch class="mobile" position="topright" colorBg="blue" colorLarge="black" colorSmall="white" />
   <TileHorizontal class="mobile" colorTop="white" colorBottom="black" />
-  <TileArch class="desktop" colorBg="red" />
-  <TileArch class="desktop" colorBg="red" />
+  <TileArch class="tablet" position="bottomleft" colorBg="blue" colorLarge="white" colorSmall="black" />
+  <TileArch class="desktop" position="bottomright" colorBg="black" colorLarge="red" />
+  <TileArch class="desktop" position="bottomleft" colorBg="blue" colorLarge="black" />
 
   <!-- Row 3 -->
-  <TileArch class="desktop" colorBg="red" />
-  <TileArch class="mobile" position="topright" colorBg="black" colorLarge="blue" colorSmall="blue" />
-  <TileArch class="mobile" position="bottomright" colorBg="white" colorLarge="black" colorSmall="black" />
-  <TileArch class="desktop" colorBg="red" />
-  <TileArch class="desktop" colorBg="red" />
+  <TileArch class="desktop" position="bottomright" colorBg="yellow" colorLarge="black" />
+  <TileArch class="tablet" position="bottomright" colorBg="black" colorLarge="red" />
+  <TileArch class="mobile" position="topright" colorBg="black" colorLarge="blue" />
+  <TileArch class="mobile" position="bottomright" colorBg="white" colorLarge="black" />
+  <TileArch class="tablet" position="topright" colorBg="black" colorLarge="red" />
+  <TileArch class="desktop" position="bottomright" colorBg="red" colorLarge="white" />
+  <TileArch class="desktop" position="topleft" colorBg="black" colorLarge="white" />
 </header>
 
-<Fluff> 
-  <TileArch class="tablet" />
-  <TileArch class="tablet" />
-  <TileArch class="tablet" />
-</Fluff>
-</div>
-
 <style>
-  div, header {
+  header {
     display: flex;
     flex-wrap: wrap;
   }
 
   @media only screen and (min-width: 430px) {
-    header {
-      width: 50vw;
-    }
   }
 
   @media only screen and (min-width: 992px) {
-    header {
-      width: 71.4285714286vw;
-    }
   }
+
 </style>
