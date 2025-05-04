9b5a8ae2100187c9f5d0cb7472347daabe85aaad
Robin <robin.django@gmail.com>
2025-05-04 14:11:02 +0200

    Starting over

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
 <script>
-  // import Articles from "$lib/components/Articles.svelte";
-  import Header from "$lib/components/molecules/Header.svelte"
-  import "$lib/css/global.css";
 </script>
-
-<Header />
-<!-- <Articles /> -->
